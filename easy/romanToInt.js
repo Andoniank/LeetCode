223406150a0aff2fa7edd3313838972ba1a5dcc2
Andoniank <andoniank@gmail.com>
2024-02-01 22:38:34 -0800

    test 1

--- easy/romanToInt.js
+++ easy/romanToInt.js
@@ -1,7 +1,16 @@
 const romanToInt = function(s) {
-    
+    let count = 0
+    let nums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    for (let i = 0; i < s.lengthl; i++) {
+        let current = s[i]
+        let previous = s[i - 1]
+        count += current
+    }
+
+    return count
 };
 
-console.log(romanToInt("III"))
-console.log(romanToInt("LVIII"))
-console.log(romanToInt("MCMXCIV"))
+console.log(romanToInt("III"))       //3
+console.log(romanToInt("LVIII"))     //58
+console.log(romanToInt("MCMXCIV"))   //1994
