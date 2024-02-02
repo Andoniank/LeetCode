c6e6735406f8eef8e5c7d2131f265ebdcf67020e
Andoniank <andoniank@gmail.com>
2024-02-01 23:03:02 -0800

    Add longestCommonPrefix

---
+++ easy/longestCommonPrefix.js
@@ -0,0 +1,7 @@
+const longestCommonPrefix = function(strs) {
+    
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix(["dog","racecar","car"]))
+console.log(longestCommonPrefix(["there", "their", "they're"]))
