5ebc51aaa1a4e3e0399ba023d2cdfd7a76768c1d
Andoniank <andoniank@gmail.com>
2024-02-03 20:10:45 -0800

    Added some tests to this problem

--- easy/longestCommonPrefix.js
+++ easy/longestCommonPrefix.js
@@ -4,7 +4,9 @@ const longestCommonPrefix = function(strs) {
     for (let i = 0; i < strs[0].length; i++) {
         for(let j = 0; j < strs.length; j++) {
             let char = strs[j][i]
+            let char2 = strs[i][j]
             
+            return char
             
         }
     }
