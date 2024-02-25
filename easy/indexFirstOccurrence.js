cf96734f0740dfb651ecd7e016be8a033869850a
Andoniank <andoniank@gmail.com>
2024-02-24 16:21:59 -0800

    Add more test cases and description ffor problem

--- easy/indexFirstOccurrence.js
+++ easy/indexFirstOccurrence.js
@@ -1,7 +1,12 @@
+//return the index of the first occurrence of the needle(substring) in the haystack(string). 
+//If the needle does not exist within the haystack return -1
+
 const firstOccurrnece = function(haystack, needle) {
 
 }
 
 console.log(firstOccurrnece("sadbutsad", "sad"))
 console.log(firstOccurrnece("leetcode", "leeto"))
+console.log(firstOccurrnece("jisbdigb", "bdi"))
+console.log(firstOccurrnece("this is a very long string of words", "of"))
 
