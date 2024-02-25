51d0a0041b95ac4ccaae08d34cf3541a3c423e33
Andoniank <andoniank@gmail.com>
2024-02-24 16:31:16 -0800

    Add more descriptions to files

--- easy/palindromeNumber.js
+++ easy/palindromeNumber.js
@@ -1,3 +1,5 @@
+//check to see if num is a paledrome same number if the indiviual indexes are written in reverse)
+
 function palindromeNumber(num) {
     let reverse = num.toString().split("").reverse().join("")
     let reverseNum = parseInt(reverse)
--- easy/romanToInt.js
+++ easy/romanToInt.js
@@ -1,3 +1,5 @@
+//write a function that returns Roman Numerals(strings) as numbers
+
 const romanToInt = function(s) {
     let count = 0
     let nums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
