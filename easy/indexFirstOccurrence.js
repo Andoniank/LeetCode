49121dfca4ea34b9324c37d642a15c3c7499a5b9
Andoniank <andoniank@gmail.com>
2024-02-24 16:28:25 -0800

    Add solution

--- easy/indexFirstOccurrence.js
+++ easy/indexFirstOccurrence.js
@@ -2,7 +2,18 @@
 //If the needle does not exist within the haystack return -1
 
 const firstOccurrnece = function(haystack, needle) {
-
+    if (!needle) return 0;
+    for (let i = 0; i < haystack.length; i++) {
+        let isMatch = true;
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                isMatch = false;
+                break;
+            }
+        }
+        if (isMatch) return i;
+    }
+    return -1;
 }
 
 console.log(firstOccurrnece("sadbutsad", "sad"))
