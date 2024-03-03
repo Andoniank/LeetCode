08925351142e1014460f7975fba8d6e852a9045f
Andoniank <andoniank@gmail.com>
2024-03-02 21:35:24 -0800

    add problem and description along with expected solutions

---
+++ easy/plusOne.js
@@ -0,0 +1,10 @@
+//given an array of integers that act as a single large integer if combined, increment the combined arrays integer by 1
+// and return the new array of integers 
+
+const plusOne = function(digits) {
+    
+};
+
+console.log(plusOne([1,2,3]))    //expected: [1,2,4]
+console.log(plusOne([4,3,2,1]))  //expected: [4,3,2,2]
+console.log(plusOne([9]))        //expected: [1,0]
