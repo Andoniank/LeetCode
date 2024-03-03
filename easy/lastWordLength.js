d2e21918bd51f0dcaa3ca4a41564e756c9ad65bd
Andoniank <andoniank@gmail.com>
2024-03-02 21:10:20 -0800

    add last world length to easy problems

---
+++ easy/lastWordLength.js
@@ -0,0 +1,9 @@
+//return the length of the last word in the string
+
+const lengthOfLastWord = function(s) {
+    
+};
+
+console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord("   fly me   to   the moon  "))
+console.log(lengthOfLastWord("luffy is still joyboy"))
