2d101b130e6006a8c3454be5819238d93f7978d6
Andoniank <andoniank@gmail.com>
2024-03-02 21:18:56 -0800

    add solution

--- easy/lastWordLength.js
+++ easy/lastWordLength.js
@@ -1,7 +1,13 @@
 //return the length of the last word in the string
 
 const lengthOfLastWord = function(s) {
-    
+    let words = s.split(" ")
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        if (words[i] !== "") {
+            return words[i].length
+        }
+    }
 };
 
 console.log(lengthOfLastWord("Hello World"))
