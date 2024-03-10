8eda368583e979fdc89e9a09d1756a1da49fdd92
Andoniank <andoniank@gmail.com>
2024-03-09 21:00:29 -0800

    added more test cases and code descriptions

--- easy/singleNumber.js
+++ easy/singleNumber.js
@@ -1,11 +1,11 @@
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 const singleNumber = function(nums) {
-    let copy = nums.sort((a, b) => a - b); 
+    let copy = nums.sort((a, b) => a - b);            // make a copy of the array and sort it
 
-    for (let i = 0; i <= nums.length - 1; i += 2) { 
-      if (copy[i] !== copy[i + 1]) {
-        return copy[i];
+    for (let i = 0; i <= nums.length - 1; i += 2) {   // loop through the array while iterating at 2
+      if (copy[i] !== copy[i + 1]) {                  // check to see if the current iterration and the next are the same
+        return copy[i];                               // if not return that number
       }
     }
   
@@ -14,6 +14,8 @@ const singleNumber = function(nums) {
 
 
 
-console.log(singleNumber([2,2,1]))
-console.log(singleNumber([4,1,2,1,2]))
-console.log(singleNumber([1]))
+console.log(singleNumber([2,2,1]))              // 1
+console.log(singleNumber([4,1,2,1,2]))          // 4
+console.log(singleNumber([1]))                  // 1
+console.log(singleNumber([1,3,5,7,2,7,5,3,1]))  // 2
+console.log(singleNumber([1,2,3,2,3,1,5,5,6]))  // 6
