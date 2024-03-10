5cf0cef0ece0a788b247cb3c1157a6bf96e27c5a
Andoniank <andoniank@gmail.com>
2024-03-09 21:03:12 -0800

    add valid palindrome and test cases

---
+++ easy/validPalindrome.js
@@ -0,0 +1,12 @@
+// check to see if the string is a valid palindrome
+// a valid palindrome is the same forwards and backwards after removing all non-alphabetic characters such as spaces and commas
+
+
+const isPalindrome = function(s) {
+    
+};
+
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
+console.log(isPalindrome("race a car"))
+console.log(isPalindrome(" "))
