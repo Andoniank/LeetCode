3bf623e8db8fdf08a1836d2be807dac9bb2935d8
Andoniank <andoniank@gmail.com>
2024-03-09 20:57:06 -0800

    Add solution

--- easy/singleNumber.js
+++ easy/singleNumber.js
@@ -1,7 +1,15 @@
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 const singleNumber = function(nums) {
-    
+    let copy = nums.sort((a, b) => a - b); 
+
+    for (let i = 0; i <= nums.length - 1; i += 2) { 
+      if (copy[i] !== copy[i + 1]) {
+        return copy[i];
+      }
+    }
+  
+    return copy[copy.length - 1];  
 };
 
 
