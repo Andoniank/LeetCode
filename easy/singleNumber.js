65682d5534b2b6a4541586202610c44b19526324
Andoniank <andoniank@gmail.com>
2024-03-09 20:44:35 -0800

    Add single Number file, descrition and test cases

---
+++ easy/singleNumber.js
@@ -0,0 +1,11 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+const singleNumber = function(nums) {
+    
+};
+
+
+
+console.log(singleNumber([2,2,1]))
+console.log(singleNumber([4,1,2,1,2]))
+console.log(singleNumber([1]))
