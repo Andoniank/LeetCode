73940a52e3bac8755d93f3f727ccc369ac124098
Andoniank <andoniank@gmail.com>
2024-03-14 23:46:46 -0700

    Add containsDuplicates with test cases and description

---
+++ easy/containsDuplicates.js
@@ -0,0 +1,10 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, 
+// and return false if every element is distinct.
+
+const containsDuplicates = function(nums) {
+
+}
+
+console.log(containsDuplicates([1,2,3,1]))
+console.log(containsDuplicates([1,2,3,4]))
+console.log(containsDuplicates([1,1,1,3,3,4,3,2,4,2]))
