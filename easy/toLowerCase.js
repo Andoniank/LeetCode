a37eb25964765abe7034907f994455fa50c6481d
Andoniank <andoniank@gmail.com>
2024-04-13 15:15:40 -0700

    Add toLowerCase problem and test cases

---
+++ easy/toLowerCase.js
@@ -0,0 +1,9 @@
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+const toLowerCase = function(s) {
+    
+}
+
+console.log(toLowerCase("Hello"))
+console.log(toLowerCase("here"))
+console.log(toLowerCase("LOVELY"))
