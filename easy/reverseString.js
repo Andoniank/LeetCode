6283f998637722e5257d8b919d6bd64d941121e9
Andoniank <andoniank@gmail.com>
2024-04-13 14:35:33 -0700

    Add solution to reverseString

--- easy/reverseString.js
+++ easy/reverseString.js
@@ -3,7 +3,19 @@
 // You must do this by modifying the input array in-place with O(1) extra memory.
 
 const reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
 
+    while (left < right) {
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+    
+    return s
 }
 
 console.log(reverseString(["h","e","l","l","o"]))
