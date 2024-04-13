e3078de02465e1003e3500ad548e482dad7889d9
Andoniank <andoniank@gmail.com>
2024-04-13 15:18:24 -0700

    add solution to toLoweCase

--- easy/toLowerCase.js
+++ easy/toLowerCase.js
@@ -1,7 +1,13 @@
 // Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
 
 const toLowerCase = function(s) {
-    
+    let result = ""
+
+    for (let i = 0; i < s.length; i++) {
+        result += s[i].toLowerCase()
+    }
+
+    return result
 }
 
 console.log(toLowerCase("Hello"))
