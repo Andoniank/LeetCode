82948474e78ff843cb98844c99cc68e566404847
Andoniank <andoniank@gmail.com>
2024-04-13 14:02:32 -0700

    Add reverseString problem and test cases

---
+++ easy/reverseString.js
@@ -0,0 +1,12 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+const reverseString = function(s) {
+
+}
+
+console.log(reverseString(["h","e","l","l","o"]))
+console.log(reverseString(["H","a","n","n","a","h"]))
+console.log(reverseString(["C", "h", "r", "i", "s"]))
+console.log(reverseString(["T", "a", "l", "e", "e", "n"]))
