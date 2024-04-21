0580797f84a7fea7672c2dd223ffddc817b930d5
Andoniank <andoniank@gmail.com>
2024-04-20 19:45:18 -0700

    add solution to smallestEvenMultiple

--- easy/smallestEvenMultiple.js
+++ easy/smallestEvenMultiple.js
@@ -1,7 +1,7 @@
 // Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
 
 const smallestEvenMultiple = function(n) {
-
+    return n % 2 === 0 ? n : n * 2
 }
 
 console.log(smallestEvenMultiple(5))
