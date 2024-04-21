c1cb9a9eb4c31fcbc956d4db2f78ddeaebefeb7f
Andoniank <andoniank@gmail.com>
2024-04-20 19:16:20 -0700

    add solution to convertTemperature

--- easy/convertTemperature.js
+++ easy/convertTemperature.js
@@ -7,7 +7,9 @@
 // Fahrenheit = Celsius * 1.80 + 32.00
 
 const convertTemperature = function(celsius) {
-
+    let kelvin = celsius + 273.15
+    let fahrenheit = (celsius * 1.80 + 32.00)
+    return [kelvin, fahrenheit]
 }
 
 console.log(convertTemperature(36.50))
