689afdd52b126f7b4be5721225a40acd4c3816bb
Andoniank <andoniank@gmail.com>
2024-04-20 19:03:48 -0700

    add getConcatenation problem and test cases

---
+++ easy/concatArrays.js
@@ -0,0 +1,12 @@
+// Given an integer array nums of length n, you want to create an array ans of length 2n 
+// where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+// Specifically, ans is the concatenation of two nums arrays.
+// Return the array ans.
+
+const getConcatenation = function(nums) {
+
+}
+
+console.log(getConcatenation([1,2,1]))
+console.log(getConcatenation([1,3,2,1]))
+console.log(getConcatenation([4,5,2,6,2,1,6,2,178]))
